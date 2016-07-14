fb47795c3bc47acd32385fe30cde45646ed86e2f
Martin Banks <anotherbanksy@gmail.com>
2016-07-14 23:27:28 +1000

    set up and use firebase to create todo list app

--- app.js
+++ app.js
@@ -1,4 +1,125 @@
 // TODO: Paste the Firebase initialization code first
-
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCbYViXzophYyXOpvpf0nLYYtamQAlzzTg",
+	authDomain: "martin-banks-sandbox.firebaseapp.com",
+	databaseURL: "https://martin-banks-sandbox.firebaseio.com",
+	storageBucket: "",
+};
+firebase.initializeApp(config);
 // TODO: Your code below
 
+// write to database
+/*
+firebase.database().ref('tasks/').push({
+  title: 'Learn CRUD',
+  done: true
+});
+
+firebase.database().ref('tasks/').set({
+  abc123: {
+    title: 'Overwrite data',
+    done: true
+  }
+});
+
+firebase.database().ref('tasks/xyz890/').set({
+  title: 'Set new data',
+  done: true
+});
+firebase.database().ref('tasks/xyz890/anothertask').set({
+  title: 'Another new data',
+  done: false
+});
+
+firebase.database().ref('tasks/xyz890/anothertask').set({
+  title: 'Another new data',
+  done: true
+});
+
+firebase.database().ref('tasks/xyz890').update({
+  title: 'Update a value'
+});
+
+*/
+
+
+
+/*
+// reading from database
+// once gets data once
+// this return a promise so can be chained with .then()
+firebase.database().ref('tasks/').once('value').then(function(snapshot) {
+  console.log(i, snapshot.val());
+}).then(()=>{
+	// this will execute when the promise has finished
+})
+
+// --- or ---
+// on gets data every time there is achange on the database
+// not a promise, can't be chained
+firebase.database().ref('tasks/').on('value', function(snapshot) {
+  console.log(i, snapshot.val());
+  i++
+})
+*/
+
+// finalmessage 
+const finalNoteText = 'No more notes!!'
+
+const sendUpdate = ()=>{
+	let content = document.getElementById('new-item').value;
+	document.getElementById('new-item').value = '';
+	firebase.database().ref('tasks/savedTasks/').push({
+	  note: content
+	});
+}
+
+delegate('button', 'click', 'button', ()=>{
+	sendUpdate()
+});
+delegate('input', 'keypress', 'input', ()=>{
+	if (event.keyCode == 13){
+		event.preventDefault();
+		sendUpdate()
+	}
+})
+
+// render tempalte to DOM
+const renderUpdate = (source)=>{
+	if ( !!source ){
+		let notecontent = source;
+		var noteKeys = Object.keys(notecontent);
+		console.log(noteKeys)
+		var displayContent = noteKeys.map((v,i,a)=>{
+			console.log(notecontent[v].note)
+			return `<li data-key='${v}'>${notecontent[v].note}</li>`
+		}).join('');
+		document.querySelector('ul').innerHTML = displayContent;
+	} else {
+		document.querySelector('ul').innerHTML = finalNoteText;
+	}	
+}
+
+// onload
+firebase.database().ref('tasks/savedTasks/').once('value').then(function(snapshot) {
+	console.log('once', snapshot.val())
+	renderUpdate( snapshot.val() )
+})
+
+// on every note change
+firebase.database().ref('tasks/savedTasks/').on('value', function(snapshot) {
+	console.log('on', snapshot.val())
+	renderUpdate( snapshot.val() )
+})
+
+delegate('ul', 'click', 'li', ()=>{ // remove on list click
+	console.log(event.target.getAttribute('data-key'));
+	let removeMe = 'tasks/savedTasks/' + event.target.getAttribute('data-key');
+	if ( event.target.parentNode.children.length > 1 ){
+		firebase.database().ref(removeMe).remove()
+	} else {
+		console.log('last note!!!');
+		firebase.database().ref(removeMe).remove()
+	}
+});
